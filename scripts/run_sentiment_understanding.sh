7bfa830d3d88a8c9dc89c85d4c84c35b9034acad
Laip11 <1623007232@qq.com>
2025-04-14 00:09:22 +0800

    update

--- scripts/run_sentiment_understanding.sh
+++ scripts/run_sentiment_understanding.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python sentiment_understanding.py \
-        --model_path /nfsdata/laip/model/LLM-Research/Meta-Llama-3___1-8B-Instruct \
+        --model_path LLM-Research/Meta-Llama-3.1-8B-Instruct \
         --valid_data_path results/valid/Meta-Llama-3___1-8B-Instruct_logits.json 
