99f7c8e9d0729af92b133eebd057f9228fb797a6
Laip11 <1623007232@qq.com>
2025-04-13 23:43:08 +0800

    update

--- scripts/run_self_knowledge.sh
+++ scripts/run_self_knowledge.sh
@@ -1,12 +1,3 @@
-list=( \
-    "Shanghai_AI_Laboratory/internlm3-8b-instruct" \
-    "/LLM-Research/Llama-3.1-Tulu-3-8B" \
-    "/LLM-Research/Meta-Llama-3.1-8B-Instruct" \
-    "/LLM-Research/Mistral-7B-Instruct-v0.3" \
-)
-
-for item in "${list[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python src/unknow_level.py \
-        --model_path $item \
-        --in_file data/unknow/prompt_unknow.json
-done
+CUDA_VISIBLE_DEVICES=0 python self-knowledge.py \
+    --model_path LLM-Research/Meta-Llama-3.1-8B-Instruct \
+    --in_file data/unknow/prompt_unknow.json
